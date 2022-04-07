0724148b20dcff062d25d7dec16cfb4ae104e876
Alexia Auddino <alexia.auddino@student.isae-supaero.fr>
2022-04-07 20:12:05 +0200

    5e partie - Tests automatises

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,13 +3,15 @@ from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -18,3 +20,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    
